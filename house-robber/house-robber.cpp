651dab9c4dc1653660c5beca934e666940016609
TRIVENDRAM PAL <43501679+tri10256@users.noreply.github.com>
2022-01-25 23:11:30 +0530

    Time: N/A, Memory: N/A',
      },
      questionId: '198',
      submissionId: '627671271',
      sessionId: '2815897',
      getLangDisplay: 'cpp',
      submissionCode: 'class Solution {\u000Apublic:\u000A    \u000A   int cache[105]\u003B\u000A    \u000A    int money_theft(vector\u003Cint\u003E\u0026nums,int i){\u000A        \u000A        if(i \u003D\u003D 0)\u000A            return nums[0]\u003B\u000A        if(i \u003D\u003D 1)\u000A            return max(nums[0],nums[1])\u003B\u000A        \u000A        if(cache[i] !\u003D \u002D1)\u000A            return cache[i]\u003B\u000A         \u000A        int theft \u003D money_theft(nums,i\u002D2) + nums[i]\u003B\u000A        int notheft \u003D money_theft(nums,i\u002D1)\u003B\u000A        \u000A        return cache[i] \u003D max(notheft,theft)\u003B\u000A    }\u000A    \u000A    \u000A    int rob(vector\u003Cint\u003E\u0026 nums) {\u000A      int n \u003D nums.size()\u003B   \u000A      memset(cache,\u002D1,sizeof(cache))\u003B\u000A        return money_theft(nums,n\u002D1)\u003B\u000A    }\u000A}\u003B',
      editCodeUrl: '/explore/item/4148',
      checkUrl: '/submissions/detail/627671271/check/',
      runtimeDistributionFormatted: '{\u0022lang\u0022: \u0022cpp\u0022, \u0022distribution\u0022: [[\u00220\u0022, 55.824], [\u00222\u0022, 0.8179], [\u00223\u0022, 7.2301], [\u00224\u0022, 28.517], [\u00225\u0022, 1.2697]]}',
      memoryDistributionFormatted: '{\u0022lang\u0022: \u0022cpp\u0022, \u0022distribution\u0022: [[\u00227400\u0022, 0.1456], [\u00227500\u0022, 5.4674], [\u00227600\u0022, 21.9517], [\u00227700\u0022, 30.0594], [\u00227800\u0022, 20.7081], [\u00227900\u0022, 16.3536]]}',
      langs: [
        
      ],
      runtime: 'None',
      memory: 'None',
      enableMemoryDistribution: 'True',
      nonSufficientMsg: 'Sorry. We do not have enough accepted submissions to show distribution chart. - LeetHub

---
+++ house-robber/house-robber.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    
+   int cache[105];
+    
+    int money_theft(vector<int>&nums,int i){
+        
+        if(i == 0)
+            return nums[0];
+        if(i == 1)
+            return max(nums[0],nums[1]);
+        
+        if(cache[i] != -1)
+            return cache[i];
+         
+        int theft = money_theft(nums,i-2) + nums[i];
+        int notheft = money_theft(nums,i-1);
+        
+        return cache[i] = max(notheft,theft);
+    }
+    
+    
+    int rob(vector<int>& nums) {
+      int n = nums.size();   
+      memset(cache,-1,sizeof(cache));
+        return money_theft(nums,n-1);
+    }
+};
