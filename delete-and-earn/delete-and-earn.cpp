4c5b5b310f3a709027f3a7c6792076799d164fb7
TRIVENDRAM PAL <43501679+tri10256@users.noreply.github.com>
2022-01-26 16:27:02 +0530

    Time: 248 ms, Memory: 44.5 MB - LeetHub

---
+++ delete-and-earn/delete-and-earn.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    unordered_map<int,int>cache;
+    
+    int recur(vector<int>&freq,int i){
+        if(i>=freq.size())
+            return 0;
+        
+        if(cache.count(i))
+            return cache[i];
+        
+        int ans1 = recur(freq,i+1);
+        int ans2 = i*freq[i] + recur(freq,i+2);
+        
+       if(!cache.count(i))
+        cache[i] =  max(ans1,ans2);
+        
+       return cache[i];
+           
+    }
+    
+    int deleteAndEarn(vector<int>& nums) {
+        
+        vector<int>freq(10004,0);
+        
+        for(int i = 0;i<nums.size();i++)
+            freq[nums[i]]++;
+        
+        return recur(freq, 0);
+    }
+};
