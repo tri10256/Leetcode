7d9fab935723f51d3b4f43bf2840e10acb67add4
TRIVENDRAM PAL <43501679+tri10256@users.noreply.github.com>
2022-02-05 04:08:05 +0530

    Time: 144 ms (60.98%), Space: 83.8 MB (86.89%) - LeetHub

--- 525-contiguous-array/525-contiguous-array.cpp
+++ 525-contiguous-array/525-contiguous-array.cpp
@@ -1,27 +1,21 @@
 class Solution {
 public:
     int findMaxLength(vector<int>& nums) {
-        int n0 = 0;
-        int n1 = 0;
+      
         for(int i = 0;i<nums.size();i++){
             if(nums[i]==0){
-                n0++;
                 nums[i]=-1;
-            }else{
-                n1++;
             }
         }
-        if(n0 == n1)
-            return 2*n0;
+       
         
         int sum =0;
-            // sum->i  sum->j ( leng - j-i )
+           
         unordered_map<int,int>map;
         map[0] = -1;
-        
         int len = 0;
+        
         for(int i = 0;i<nums.size();i++){
-            
             sum+=nums[i];
             if(!map.count(sum)){
                 map[sum]=i;
