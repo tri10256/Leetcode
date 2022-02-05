7f20798c4607998e68152d877c763f3fbcdfea66
TRIVENDRAM PAL <43501679+tri10256@users.noreply.github.com>
2022-02-06 00:43:40 +0530

    Time: 8 ms (26.14%), Space: 8.8 MB (50.21%) - LeetHub

--- 162-find-peak-element/162-find-peak-element.cpp
+++ 162-find-peak-element/162-find-peak-element.cpp
@@ -1,14 +1,18 @@
 class Solution {
 public:
     int findPeakElement(vector<int>& nums) {
-    
+     int l = 0;
+     int h = nums.size() -1;
+     while(l<h){ 
+         int mid = l+(h-l)/2;
+         
+         if(nums[mid]>nums[mid+1]){
+             h = mid;
+         }else{
+             l = mid+1;
+         }
+     }
        
-        for(int i = 0;i<nums.size()-1;i++){
-            if(nums[i]>nums[i+1])
-                return i;
-        }
-        
-        return nums.size()-1;
-        
+    return l;
     }
 };
