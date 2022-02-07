bc4431eb5d3ad20bc63405e50cdf392d263f81c1
TRIVENDRAM PAL <43501679+tri10256@users.noreply.github.com>
2022-02-07 11:45:42 +0530

    Time: 63 ms (26.24%), Space: 30.9 MB (87.56%) - LeetHub

--- 1306-jump-game-iii/1306-jump-game-iii.cpp
+++ 1306-jump-game-iii/1306-jump-game-iii.cpp
@@ -4,11 +4,9 @@ public:
     bool dfs(vector<int>&arr,int i,int n,vector<bool>&vis){
         if(i<0 || i>=n || vis[i] !=0)
             return false;
-        
-        vis[i] = 1;
-        
         if(arr[i] == 0)
             return true;
+         vis[i] = 1;
         
         bool ans1 = dfs(arr,i-arr[i],n,vis);
         bool ans2 = dfs(arr,i+arr[i],n,vis);
