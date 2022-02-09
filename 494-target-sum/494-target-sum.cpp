cf6685c9c62a6da11b098c93209953ac18f3f1f8
TRIVENDRAM PAL <43501679+tri10256@users.noreply.github.com>
2022-02-09 12:54:43 +0530

    Time: 57 ms (46.88%), Space: 13.3 MB (35.75%) - LeetHub

---
+++ 494-target-sum/494-target-sum.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    int dp[1001][1001];
+    
+    int solve(vector<int>&nums,int target,int sum,int n){
+        if(n==0){
+            if(sum == target || sum == -target){
+                dp[n][abs(sum)] = 1;
+                return dp[n][abs(sum)];
+            }else{
+                dp[n][abs(sum)] = 0;
+                return dp[n][abs(sum)];
+            }
+        }
+        
+        if(dp[n][abs(sum)]!=-1)
+            return dp[n][abs(sum)];
+        
+        dp[n][abs(sum)] = solve(nums,target,sum+nums[n-1],n-1) + solve(nums,target,sum-nums[n-1],n-1);
+        
+        return dp[n][abs(sum)];
+    }
+    
+    int findTargetSumWays(vector<int>& nums, int target) {
+        memset(dp,-1,sizeof(dp));
+        int ans = solve(nums,target,0,nums.size());
+        if(target == 0)
+            return ans;
+        
+        return ans/2;
+    }
+};
