d1d7209e2293785c922f9d881b22b1b849570fc9
TRIVENDRAM PAL <43501679+tri10256@users.noreply.github.com>
2022-02-10 22:11:39 +0530

    Time: 17 ms (51.06%), Space: 9.3 MB (15.79%) - LeetHub

---
+++ 72-edit-distance/72-edit-distance.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+     
+    vector<vector<int>>dp;
+    int lcs(string &word1, string &word2,int n, int m){
+       if(n==0) return dp[n][m]= m;
+       if(m==0) return dp[n][m]=n;
+        
+        if(dp[n][m]!=INT_MAX)
+            return dp[n][m];
+        
+        if(word1[n-1] == word2[m-1]){
+           return dp[n][m] = lcs(word1,word2,n-1,m-1);
+        }
+            
+        
+            int c1 = lcs(word1,word2,n,m-1); //insert
+            int c2 = lcs(word1,word2,n-1,m); //delete
+            int c3 = lcs(word1,word2,n-1,m-1); //replace
+            
+          return dp[n][m] =  1+ min({c1,c2,c3});
+     }
+    
+    int minDistance(string word1, string word2) {
+        int n = word1.size();
+        int m = word2.size();
+        
+        dp.assign(n+1,vector<int>(m+1,INT_MAX));
+      
+        int len = lcs(word1,word2,n,m);  
+        
+        return len;
+    }
+};
