9a73a596cf6101d1fa63ffaf20a292c0e68bf9e2
TRIVENDRAM PAL <43501679+tri10256@users.noreply.github.com>
2022-02-11 00:34:53 +0530

    Time: 0 ms (100.00%), Space: 8.3 MB (87.36%) - LeetHub

--- 145-binary-tree-postorder-traversal/145-binary-tree-postorder-traversal.cpp
+++ 145-binary-tree-postorder-traversal/145-binary-tree-postorder-traversal.cpp
@@ -13,18 +13,47 @@
 class Solution {
 public:
   
-    void postorder(TreeNode *root,vector<int>&ans){
-        if(!root)
-        return;
-       postorder(root->left,ans);
-       postorder(root->right,ans);
+//     void postorder(TreeNode *root,vector<int>&ans){
+//         if(!root)
+//         return;
+//        postorder(root->left,ans);
+//        postorder(root->right,ans);
         
-        ans.push_back(root->val);
+//       ans.push_back(root->val);
+//     }
+    
+    void post(TreeNode *root,vector<int>&ans){
+        
+        if(!root)
+            return;
+        
+       stack<TreeNode*>main;
+       stack<TreeNode*>alt;
+       main.push(root);
+        
+        while(!main.empty()){
+            TreeNode *node = main.top();
+            main.pop();
+            alt.push(node);
+            
+            if(node->left)
+                main.push(node->left);
+            if(node->right)
+                main.push(node->right);
+        }
+        
+        while(!alt.empty()){
+            ans.push_back(alt.top()->val);
+            alt.pop();
+        }
+        
+       
     }
+    
     vector<int> postorderTraversal(TreeNode* root) {
-         vector<int>ans;
+        vector<int>ans;
           
-        postorder(root,ans);
+        post(root,ans);
         return ans;
     }
 };
