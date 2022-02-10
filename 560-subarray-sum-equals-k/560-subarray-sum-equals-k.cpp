58471a1633c400452b6d948e9e4ddb67c43710c2
TRIVENDRAM PAL <43501679+tri10256@users.noreply.github.com>
2022-02-10 17:27:35 +0530

    Time: 117 ms (48.04%), Space: 36.1 MB (64.64%) - LeetHub

--- 560-subarray-sum-equals-k/560-subarray-sum-equals-k.cpp
+++ 560-subarray-sum-equals-k/560-subarray-sum-equals-k.cpp
@@ -6,7 +6,17 @@ public:
         int n = nums.size();
         unordered_map<int,int>map;
         /*
-        sum(i,j)=sum(0,j)-sum(0,i), where sum(i,j) represents the sum of all the elements from index i to j-1. Can we use this property to optimize it.*/
+        sum(i,j)=sum(0,j)-sum(0,i), where sum(i,j) represents the sum of all the elements from index i to j-1. Can we use this property to optimize it.
+        
+        subarray sum = sum(i,j) that is given = k;
+        sum(0,j) = we are calculating by moving in array;
+        we don't know sum (0,i)= x;
+        
+        equations
+        sum(i,j) = sum(0,j) - x;
+        x = sum(0,j) - sum(i,j);
+        x = sum(0,j) - k;
+        */
         
         map[0] = 1;
         for(int j = 0;j<n;j++){
