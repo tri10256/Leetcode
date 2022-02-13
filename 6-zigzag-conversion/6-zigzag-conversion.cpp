3143d111e422c10a9caeaa52e84cee7055c9e959
TRIVENDRAM PAL <43501679+tri10256@users.noreply.github.com>
2022-02-13 18:11:39 +0530

    Time: 3 ms (99.00%), Space: 10.8 MB (39.99%) - LeetHub

---
+++ 6-zigzag-conversion/6-zigzag-conversion.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    string convert(string s, int n) {
+      int sz = s.size();
+      if(n == 1) return s;
+        
+        vector<string>rows(min(sz,n));
+        int curRow = 0;
+        bool godown = false;
+        
+        for(char c:s){
+            rows[curRow]+=c;
+            if(curRow == 0 || curRow == n-1){
+                godown = !godown;
+            }
+            
+          curRow+=godown?1:-1;
+        }
+        
+        string ret;
+        for(string str:rows){
+            ret+=str;
+        }
+        
+        return ret;
+    }
+};
