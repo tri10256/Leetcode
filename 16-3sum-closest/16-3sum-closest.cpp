1af3c949f2381351ad9514b640d69b751d60c54c
TRIVENDRAM PAL <43501679+tri10256@users.noreply.github.com>
2022-02-15 16:41:17 +0530

    Time: 25 ms (29.41%), Space: 10.1 MB (7.42%) - LeetHub

--- 16-3sum-closest/16-3sum-closest.cpp
+++ 16-3sum-closest/16-3sum-closest.cpp
@@ -1,26 +1,29 @@
 class Solution {
 public:
     int threeSumClosest(vector<int>& nums, int target) {
-    if(nums.size() < 3) return 0;
-    int closest = nums[0]+nums[1]+nums[2];
-    sort(nums.begin(), nums.end());
-    for(int first = 0 ; first < nums.size()-2 ; ++first) {
-      //  if(first > 0 && nums[first] == nums[first-1]) continue;
-        int second = first+1;
-        int third = nums.size()-1;            
-        while(second < third) {
-            int curSum = nums[first]+nums[second]+nums[third];
-            if(curSum == target) return curSum;
-            if(abs(target-curSum)<abs(target-closest)) {
-                closest = curSum;
-            }
-            if(curSum > target) {
-                --third;
-            } else {
-                ++second;
+       
+        int n = nums.size();
+        int closest = nums[0] + nums[1] +nums[2];
+        sort(nums.begin(),nums.end());
+        
+        for(int i = 0;i<n-2;i++){
+            int j = i+1;
+            int k = n-1;
+            while(j<k){
+                int sum = nums[i] + nums[j] + nums[k];
+                
+                if(sum == target)
+                    return sum;
+                 if(abs(target-sum )< abs(target- closest))
+                    closest = sum;
+                if(sum>target)
+                    k--;
+                else {
+                    j++;
+                }  
             }
         }
+        
+        return closest;
     }
-    return closest;
-}
 };
