3f3f8132ca0e54330ee8d72e88b6e88ece678fde
TRIVENDRAM PAL <43501679+tri10256@users.noreply.github.com>
2022-02-18 20:40:14 +0530

    Time: 4 ms (99.13%), Space: 10.5 MB (63.58%) - LeetHub

--- 300-longest-increasing-subsequence/300-longest-increasing-subsequence.cpp
+++ 300-longest-increasing-subsequence/300-longest-increasing-subsequence.cpp
@@ -4,7 +4,7 @@ public:
         vector<int>dp;
         
         for(int i =0 ;i<nums.size();i++){
-            if(!dp.size()|| dp.back()<nums[i]){
+            if(dp.size()==0|| dp.back()<nums[i]){
                 dp.push_back(nums[i]);
             }else{
                 int index = lower_bound(dp.begin(),dp.end(),nums[i])-dp.begin();
