ca2b6c0ce89832310bfda92989cb4401fe0eb1b5
TRIVENDRAM PAL <43501679+tri10256@users.noreply.github.com>
2022-02-18 17:15:34 +0530

    Time: 4 ms (75.28%), Space: 6.6 MB (63.55%) - LeetHub

--- 763-partition-labels/763-partition-labels.cpp
+++ 763-partition-labels/763-partition-labels.cpp
@@ -10,14 +10,14 @@ public:
          
         int i = 0;
         vector<int>ans;
-        int lastpos = 0;
-        int initial = 0;
+        int grlastpos = 0;
+        int grinitial = 0;
         for(i = 0;i<n;i++){
-            lastpos = max(lastpos,last[s[i]-'a']);
+            grlastpos = max(grlastpos,last[s[i]-'a']);
             
-            if(i == lastpos){
-                ans.push_back(i-initial+1);
-                initial= i+1;
+            if(i == grlastpos){
+                ans.push_back(i-grinitial+1);
+                grinitial= i+1;
             }
         }
         
