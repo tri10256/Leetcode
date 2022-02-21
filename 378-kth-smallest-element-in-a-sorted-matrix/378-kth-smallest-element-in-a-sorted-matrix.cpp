5f955b9e38878af734dcaec0a1e5434733ff0f55
TRIVENDRAM PAL <43501679+tri10256@users.noreply.github.com>
2022-02-21 19:00:48 +0530

    Time: 69 ms (24.31%), Space: 15.2 MB (13.23%) - LeetHub

---
+++ 378-kth-smallest-element-in-a-sorted-matrix/378-kth-smallest-element-in-a-sorted-matrix.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int kthSmallest(vector<vector<int>>& matrix, int k) {
+        priority_queue<int,vector<int>,greater<int>>pq;
+        for(auto x:matrix){
+            for(auto y:x){
+                pq.push(y);
+            }
+        }
+        while(k>1){
+            pq.pop();
+            k--;
+        }
+        return pq.top();
+    }
+};
