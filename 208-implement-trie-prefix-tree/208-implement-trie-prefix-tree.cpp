86e112bf6fb96445ce1575b212b6f451bab30a59
TRIVENDRAM PAL <43501679+tri10256@users.noreply.github.com>
2022-02-21 12:02:19 +0530

    Time: 97 ms (49.16%), Space: 45.1 MB (32.91%) - LeetHub

--- 208-implement-trie-prefix-tree/208-implement-trie-prefix-tree.cpp
+++ 208-implement-trie-prefix-tree/208-implement-trie-prefix-tree.cpp
@@ -1,83 +1,36 @@
-struct Node{
-   
-    Node *links[26];
-    bool flag = false;
-    
-    bool contains(char &ch){
-        if(links[ch-'a'] != NULL)
-            return true;
-        return false;   
-    }
-    void put(char &ch,Node *node){
-         links[ch-'a'] = node;
-    }
-    
-    Node* get(char &ch){
-        return links[ch-'a'];
-    }
-    void setEnd(){
-        flag = true;
-    }
-    
-    bool isEnd(){
-        return flag;
-    }
-    
-};
-
 class Trie {
-    Node *root;
 public:
-    Trie() {
-         root = new Node();
-    }
+    Trie* children[26] = {};
+    bool isWord = false;
     
     void insert(string word) {
-        
-        Node *node = root;
-        for(int i = 0;i<word.size();i++){
-            char ch = word[i];
-            if(!node->contains(ch)){
-                node->put(ch,new Node());
-            }
-            node = node->get(ch);
+        Trie* cur = this;
+        for (char c : word) {
+            c -= 'a';
+            if (cur->children[c] == nullptr)
+                cur->children[c] = new Trie();
+            cur = cur->children[c];
         }
-        node->setEnd();
+        cur->isWord = true;
     }
     
-     bool search(string word) {
-          Node *node = root;
-          for(int i = 0;i<word.size();i++){
-               char ch = word[i];
-              if(!node->contains(ch))
-                  return false;
-              
-              node = node->get(ch);
-          }
-        
-       if(node->isEnd()){
-           return true;
-       }
-        
-        return false;
-    }
-    bool startsWith(string prefix) {
-       Node *node = root;
-        for(int i = 0;i<prefix.size();i++){
-            char ch = prefix[i];
-            if(!node->contains(ch))
-                return false;
-          node = node->get(ch);     
+    bool search(string word) {
+        Trie* cur = this;
+        for (char c : word) {
+            c -= 'a';
+            if (cur->children[c] == nullptr) return false;
+            cur = cur->children[c];
         }
-        
+        return cur->isWord;
+    }
+    
+    bool startsWith(string prefix) {
+        Trie* cur = this;
+        for (char c : prefix) {
+            c -= 'a';
+            if (cur->children[c] == nullptr) return false;
+            cur = cur->children[c];
+        }
         return true;
     }
 };
-
-/**
- * Your Trie object will be instantiated and called as such:
- * Trie* obj = new Trie();
- * obj->insert(word);
- * bool param_2 = obj->search(word);
- * bool param_3 = obj->startsWith(prefix);
- */
