829a484003a321a51adf0099e0549b390207286a
TRIVENDRAM PAL <43501679+tri10256@users.noreply.github.com>
2022-02-22 11:33:09 +0530

    Time: 3 ms (58.78%), Space: 5.9 MB (92.06%) - LeetHub

--- 171-excel-sheet-column-number/171-excel-sheet-column-number.cpp
+++ 171-excel-sheet-column-number/171-excel-sheet-column-number.cpp
@@ -2,8 +2,10 @@ class Solution {
 public:
     int titleToNumber(string &s) {
         int no = 0;
-        for(auto ch:s){
-            no=no*26+(ch-'A'+1); ///base 26 similar to base 10;
+        for(auto &ch:s){
+            int d = (ch-'A'+1);
+            no*=26; ///base 26 similar to base 10;
+            no+=d;
         }
         return no;
     }
