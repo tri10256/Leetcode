4aaaaeb65ea17415727af911dea57041feee35d0
TRIVENDRAM PAL <43501679+tri10256@users.noreply.github.com>
2022-02-22 13:21:42 +0530

    Time: 11 ms (80.10%), Space: 9.3 MB (52.30%) - LeetHub

--- 1329-sort-the-matrix-diagonally/1329-sort-the-matrix-diagonally.cpp
+++ 1329-sort-the-matrix-diagonally/1329-sort-the-matrix-diagonally.cpp
@@ -1,82 +1,26 @@
 class Solution {
 public:
     vector<vector<int>> diagonalSort(vector<vector<int>>& mat) {
+        ////Done this solution with another approach also 
+        ///Lee's method
+       unordered_map<int,priority_queue<int,vector<int>,greater<int>>>mp;
         int n = mat.size();
         int m = mat[0].size();
         
-        vector<vector<int>>ans;
-        
-        for(int k = 0;k<m;k++){
-            
-            int j = k;
-            int i = 0;
-            vector<int>temp;
-            while(i<n && j<m){
-                temp.push_back(mat[i][j]);
-                i++;
-                j++;
+        for(int i  = 0;i<n;i++){
+            for(int j = 0;j<m;j++){
+                mp[i-j].push(mat[i][j]);
             }
-            ans.push_back(temp);
         }
         
-        for(int k = 1;k<n;k++){
-            
-            int i = k;
-            int j = 0;
-            vector<int>temp;
-            while(i<n && j<m){
-                temp.push_back(mat[i][j]);
-                i++;
-                j++;
+        for(int i  = 0;i<n;i++){
+            for(int j = 0;j<m;j++){
+                mat[i][j] = mp[i-j].top();
+                mp[i-j].pop();
             }
-            ans.push_back(temp);
         }
         
-        for(int i  = 0;i<ans.size();i++){
-            sort(ans[i].begin(),ans[i].end());
-        }
-         int s = 0;
-         for(int k = 0;k<m;k++){
-            
-            int j = k;
-            int i = 0;
-            auto vec = ans[s];
-             s++;
-             int t = 0;
-            while(i<n && j<m){
-                mat[i][j] = vec[t];
-               //cout<<vec[t]<<" ";
-                i++;
-                j++;
-                t++;
-            }
-            // cout<<endl;
-        }
-        
-          for(int k = 1;k<n;k++){
-            
-            int i = k;
-            int j = 0;
-            auto vec = ans[s];
-              int t = 0;
-              s++;
-            while(i<n && j<m){
-                mat[i][j] = vec[t];
-              //  cout<<vec[t]<<" ";
-                i++;
-                j++;
-                t++;
-            }
-            // cout<<endl;
-        }
-        // cout<<"temp"<<endl;
-        // for(auto &x:ans){
-        //     for(auto &y:x){
-        //         cout<<y<<" ";
-        //     }
-        //     cout<<endl;
-        // }
-        
+       
         return mat;
     }
 };
