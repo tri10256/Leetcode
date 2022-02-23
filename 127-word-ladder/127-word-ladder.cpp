97c75aa5cd9a48cf31adddb5509819b841efd667
TRIVENDRAM PAL <43501679+tri10256@users.noreply.github.com>
2022-02-23 17:36:23 +0530

    Time: 220 ms (50.46%), Space: 14 MB (72.81%) - LeetHub

--- 127-word-ladder/127-word-ladder.cpp
+++ 127-word-ladder/127-word-ladder.cpp
@@ -1,32 +1,66 @@
 class Solution {
 public:
     int ladderLength(string beginWord, string endWord, vector<string>& wordList) {
-        unordered_set<string> dict(wordList.begin(), wordList.end());
-        queue<string> todo;
-        todo.push(beginWord);
-        int ladder = 1;
-        while (!todo.empty()) {
-            int n = todo.size();
-            for (int i = 0; i < n; i++) {
-                string word = todo.front();
-                todo.pop();
-                if (word == endWord) {
-                    return ladder;
-                }
-                dict.erase(word);
-                for (int j = 0; j < word.size(); j++) {
-                    char c = word[j];
-                    for (int k = 0; k < 26; k++) {
-                        word[j] = 'a' + k;
-                        if (dict.find(word) != dict.end()) {
-                            todo.push(word);
+        /*took reference from techdose that bfs will applied in the graph for finding
+        the shortest path not dfs because becuase a graph have a probability of finding
+        exponenital no of paths from source to destination, which bfs will have fixed         shortestpaths
+          
+        ///compare will give 0for exactmatch                                                           ///1 if temp have more size than curr
+          /// -1 if curr have more size than                                                             //temp
+          
+                                                    
+         */
+        unordered_set<string>st;
+        bool isPresent = false;
+        
+        for(auto &word:wordList){
+           
+            if(word == endWord){
+                isPresent = true;
+            }
+            st.insert(word);
+        }
+        if(!isPresent){
+            return 0;
+        }
+        
+        int level = 0;
+        queue<string>q;
+        q.push(beginWord);
+        
+        while(!q.empty()){
+            level+=1;
+            int levelnodes = q.size();
+            
+            while(levelnodes--){
+                
+                string curr = q.front();
+                q.pop();
+                
+               
+                for(int i = 0;i<curr.size();++i){
+                     string temp = curr;
+                    for(char c = 'a' ;c<='z';++c){
+                          temp[i] = c;
+                        
+                        if(temp == curr){
+                            continue;
+                        }else if(temp == endWord){
+                            return level+1;
+                        }else{
+                            
+                            if(st.count(temp)){
+                                q.push(temp);
+                                st.erase(temp);
+                            }
                         }
-                     }
-                    word[j] = c;
+                        
+                    }
+    
                 }
             }
-            ladder++;
         }
+        
         return 0;
     }
 };
