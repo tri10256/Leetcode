59d95a7815daa40abfda24a29f5dd8368920f282
TRIVENDRAM PAL <43501679+tri10256@users.noreply.github.com>
2022-03-04 20:08:13 +0530

    Time: 56 ms (89.93%), Space: 21.5 MB (98.92%) - LeetHub

---
+++ 1020-number-of-enclaves/1020-number-of-enclaves.cpp
@@ -0,0 +1,65 @@
+class Solution {
+public:
+    
+    int x[4] = {1,0,-1,0};
+    int y[4] = {0,-1,0,1};
+    
+    void dfs(vector<vector<int>>&grid,int i ,int j, int n, int m){
+        
+        if(i< 0 || j< 0 || i>=n || j>=m || grid[i][j] == 0)
+            return;
+        
+        grid[i][j] = 0;
+        
+        for(int k = 0;k<4;k++){
+            int nx = i+x[k];
+            int ny = j + y[k];
+            dfs(grid,nx,ny,n,m);
+        }
+    }
+     int count = 0;
+     void dfsi(vector<vector<int>>&grid,int i ,int j, int n, int m){
+        
+        if(i< 0 || j< 0 || i>=n || j>=m || grid[i][j] == 0)
+            return;
+        
+        grid[i][j] = 0;
+        count++;
+        
+        for(int k = 0;k<4;k++){
+            int nx = i+x[k];
+            int ny = j + y[k];
+            dfsi(grid,nx,ny,n,m);
+        }
+    }
+    
+    int numEnclaves(vector<vector<int>>& grid) {
+        
+        int n = grid.size();
+        int m = grid[0].size();
+        
+        for(int j = 0;j<m;j++){
+            if(grid[0][j])
+            dfs(grid,0,j,n,m);
+             if(grid[n-1][j])
+            dfs(grid,n-1,j,n,m);
+        }
+         for(int i = 0;i<n;i++){
+            if(grid[i][0])
+            dfs(grid,i,0,n,m);
+            if(grid[i][m-1])
+            dfs(grid,i,m-1,n,m);
+        }
+        
+        for(int i = 0;i<n;i++){
+            for(int j = 0;j<m;j++){
+                //cout<<grid[i][j]<<" ";
+                if(grid[i][j])
+                   dfsi(grid,i,j,n,m);
+            }
+            //cout<<endl;
+        }
+        
+        return count;
+    }
+};
