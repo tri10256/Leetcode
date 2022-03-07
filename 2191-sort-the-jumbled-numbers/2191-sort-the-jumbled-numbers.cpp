451aa5035e48a6edbde1b38700e1b331ea08890a
TRIVENDRAM PAL <43501679+tri10256@users.noreply.github.com>
2022-03-07 09:15:13 +0530

    Time: 428 ms (85.71%), Space: 105.5 MB (28.57%) - LeetHub

--- 2191-sort-the-jumbled-numbers/2191-sort-the-jumbled-numbers.cpp
+++ 2191-sort-the-jumbled-numbers/2191-sort-the-jumbled-numbers.cpp
@@ -1,27 +1,38 @@
 class Solution {
 public:
-    #include<map>    
-    int getvalue(int n,vector<int>& mapping){
-        int value=0,t=1;
-        if(n==0)
-            return mapping[0];
-        while(n!=0){
-            int temp=n%10;            
-            value=mapping[temp]*t+value;
-            t=t*10, n=n/10;
+    
+    int getMapped(int n, vector<int>&mapping){
+        if(n == 0 )
+        return mapping[n];
+        
+        int val = 0;
+        int posMultiplyFactor = 1;
+        
+        while(n){
+            int last = n%10;
+            val = (mapping[last]*posMultiplyFactor) + val;
+            posMultiplyFactor *=10;
+            n/=10;
         }
-        return value;
-    }            
-    vector<int> sortJumbled(vector<int>& mapping, vector<int>& nums) {        
-        multimap<int,int> m;
-        for(int a:nums){            
-            int temp=getvalue(a,mapping);                        
-            m.insert(make_pair(temp,a));
-        }        
+        
+        return val;
+    }
+    vector<int> sortJumbled(vector<int>& mapping, vector<int>& nums) {
+        
+        multimap<int,int>m;
+        
+        for(auto &num:nums){
+            int formed = getMapped(num,mapping);
+            m.insert({formed,num});
+        }
+        
         nums.clear();
-        for(auto itr=m.begin();itr!=m.end();itr++){
-            nums.push_back(itr->second);
+        
+        for(auto &it:m){
+            nums.push_back(it.second);
         }
+        
         return nums;
+        
     }
 };
