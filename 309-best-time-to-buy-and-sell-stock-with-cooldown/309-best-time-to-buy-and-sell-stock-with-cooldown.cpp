b7ce9b61c6f36e98bd413c5b0919941ccecf5d60
TRIVENDRAM PAL <43501679+tri10256@users.noreply.github.com>
2022-03-10 11:47:10 +0530

    Time: 4 ms (76.35%), Space: 11.2 MB (57.37%) - LeetHub

--- 309-best-time-to-buy-and-sell-stock-with-cooldown/309-best-time-to-buy-and-sell-stock-with-cooldown.cpp
+++ 309-best-time-to-buy-and-sell-stock-with-cooldown/309-best-time-to-buy-and-sell-stock-with-cooldown.cpp
@@ -1,18 +1,14 @@
 class Solution {
 public:
-	int maxProfit(vector<int>& prices){
-		if (prices.size() <= 1) return 0;
-		vector<int> s0(prices.size(), 0);
-		vector<int> s1(prices.size(), 0);
-		vector<int> s2(prices.size(), 0);
-		s1[0] = -prices[0];
-		s0[0] = 0;
-		s2[0] = INT_MIN;
-		for (int i = 1; i < prices.size(); i++) {
-			s0[i] = max(s0[i - 1], s2[i - 1]);
-			s1[i] = max(s1[i - 1], s0[i - 1] - prices[i]);
-			s2[i] = s1[i - 1] + prices[i];
-		}
-		return max(s0[prices.size() - 1], s2[prices.size() - 1]);
-	}
+	int maxProfit(vector<int>& prices) {
+    int sold = 0, hold = INT_MIN, rest = 0;
+    for (int i=0; i<prices.size(); ++i)
+    {
+        int prvSold = sold;
+        sold = hold + prices[i];
+        hold = max(hold, rest-prices[i]);
+        rest = max(rest, prvSold);
+    }
+    return max(sold, rest);
+}
 };
