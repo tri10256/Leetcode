1318fd2a7172324bd7d9f87a0a5f2a5803c151db
TRIVENDRAM PAL <43501679+tri10256@users.noreply.github.com>
2022-03-11 09:54:05 +0530

    Time: 88 ms (22.71%), Space: 19.6 MB (25.30%) - LeetHub

--- 39-combination-sum/39-combination-sum.cpp
+++ 39-combination-sum/39-combination-sum.cpp
@@ -1,29 +1,26 @@
 class Solution {
 public:
-    
-    void nocomb(vector<int>&candidates,int target,vector<vector<int>>&vec,int j,vector<int>veci,int sum)
+    vector<vector<int>>vec;
+    void nocomb(vector<int>&candidates,int target,int j,vector<int>veci,int sum)
     {
-           if(sum>target)
-            {
+           if(sum>target){
                 return;
-            }
-            if(target==sum)
-                vec.push_back(veci);
-        for(int i=j;i<candidates.size();i++)
-        { sum+=candidates[i];
-         veci.push_back(candidates[i]);
-         nocomb(candidates,target,vec,i,veci,sum);
-         target+=candidates[i];
-         veci.pop_back();
-        }
+           }
+           if(sum == target)
+               vec.push_back(veci);
         
+            for(int i=j;i<candidates.size();i++)
+            {
+                sum+=candidates[i];
+                veci.push_back(candidates[i]);
+                nocomb(candidates,target,i,veci,sum);
+                veci.pop_back();
+                sum -=candidates[i];
+            }
     }
     vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
-         vector<vector<int>>vec;
         vector<int>veci;
-         nocomb(candidates,target,vec,0,veci,0);
-        return vec;
-        
-        
+        nocomb(candidates,target,0,veci,0);
+        return vec;   
     }
 };
