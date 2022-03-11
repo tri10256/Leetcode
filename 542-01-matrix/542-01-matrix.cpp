013e0b4ebae4fee6d4a8af0e6108593024419717
TRIVENDRAM PAL <43501679+tri10256@users.noreply.github.com>
2022-03-11 17:17:10 +0530

    Time: 162 ms (20.72%), Space: 30.3 MB (38.82%) - LeetHub

--- 542-01-matrix/542-01-matrix.cpp
+++ 542-01-matrix/542-01-matrix.cpp
@@ -1,38 +1,33 @@
 class Solution {
 public:
-    vector<vector<int>> updateMatrix(vector<vector<int>>& matrix) {
-        
-        int rows = matrix.size();
-        if (rows == 0)
-            return matrix;
-        int cols = matrix[0].size();
-        
-        vector<vector<int>> dist(rows, vector<int> (cols, INT_MAX));
+    vector<vector<int>> updateMatrix(vector<vector<int>>& grid) {
+        int m = grid.size();
+        int n = grid[0].size();
+        vector<vector<int>> dist(m, vector<int>(n, INT_MAX));
         queue<pair<int, int>> q;
-        
-        
-        for (int i = 0; i < rows; i++) {
-            for (int j = 0; j < cols; j++) {
-                if (matrix[i][j] == 0) {
+        for(int i = 0; i < m; ++i) {
+            for(int j = 0; j < n; ++j) {
+                if(grid[i][j] == 0) {
                     dist[i][j] = 0;
-                    q.push({ i, j }); //Put all 0s in the queue.
+                    q.push({i, j});
                 }
             }
         }
-
-        int dir[4][2] = { { -1, 0 }, { 1, 0 }, { 0, -1 }, { 0, 1 } };
-        
-        while (!q.empty()) {
-            pair<int, int> curr = q.front();
+        int dir[4][2] = {{-1, 0}, {0, -1}, {0, 1}, {1, 0}};
+        while(!q.empty()) {
+            int i = q.front().first;
+            int j = q.front().second;
             q.pop();
-            for (int i = 0; i < 4; i++) {
-                int new_r = curr.first + dir[i][0], new_c = curr.second + dir[i][1];
-                if (new_r >= 0 && new_c >= 0 && new_r < rows && new_c < cols) {
-                    if (dist[new_r][new_c] > dist[curr.first][curr.second] + 1) {
-                        dist[new_r][new_c] = dist[curr.first][curr.second] + 1;
-                        q.push({ new_r, new_c });
+            for(int k = 0; k < 4; ++k) {
+                int x = i + dir[k][0];
+                int y = j + dir[k][1];
+                if(x >= 0 && y >= 0 && x < m && y < n) {
+                    if(grid[x][y] == 1 && dist[x][y] > dist[i][j] + 1) {
+                        grid[x][y] = 2;
+                        dist[x][y] = dist[i][j] + 1;
+                        q.push({x, y});
                     }
-                }
+                } 
             }
         }
         return dist;
