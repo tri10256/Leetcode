a9e7f3ab63bd94fc69cd2036b1f6550fde575d0c
TRIVENDRAM PAL <43501679+tri10256@users.noreply.github.com>
2022-03-11 20:34:09 +0530

    Time: 0 ms (100.00%), Space: 7.9 MB (38.54%) - LeetHub

--- 63-unique-paths-ii/63-unique-paths-ii.cpp
+++ 63-unique-paths-ii/63-unique-paths-ii.cpp
@@ -21,9 +21,9 @@ public:
         int ans1 = 0;
         int ans2 = 0;
         
-        if(i+1<n)
+        //if(i+1<n)
          ans1 = dfs(Grid,n,m,i+1,j);
-        if(j+1<m)
+       // if(j+1<m)
         ans2 = dfs(Grid,n,m,i,j+1);
         
         return dp[i][j] = ans1+ans2;
@@ -32,29 +32,7 @@ public:
     int uniquePathsWithObstacles(vector<vector<int>>&Grid) {
         int n = Grid.size();
         int m = Grid[0].size();
-        dp.resize(n+1,vector<int>(m+1,-1));
-        
-//         if(n ==1){
-//             for(int j = 0;j<m;j++){
-//                 if(Grid[0][j] == 1)
-//                     return 0;
-//             }
-         
-//             return 1;
-//         }
-        
-//         if(m ==1){
-//             for(int i = 0;i<n;i++){
-//                 if(Grid[i][0] == 1)
-//                     return 0;
-//             }
-         
-//             return 1;
-//         }
-        
-        
-        
+        dp.resize(n+1,vector<int>(m+1,-1));        
         return dfs(Grid,n,m,0,0);
-        
     }
 };
