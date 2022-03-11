6c2ea5c2c22325fdd06993302713c39778790631
TRIVENDRAM PAL <43501679+tri10256@users.noreply.github.com>
2022-03-11 18:59:01 +0530

    Time: 4 ms (40.50%), Space: 6.5 MB (33.91%) - LeetHub

--- 1081-smallest-subsequence-of-distinct-characters/1081-smallest-subsequence-of-distinct-characters.cpp
+++ 1081-smallest-subsequence-of-distinct-characters/1081-smallest-subsequence-of-distinct-characters.cpp
@@ -1,31 +1,36 @@
 class Solution {
 public:
     string smallestSubsequence(string s) {
-        int count[26]={0};//stores the frequency of characters
-        int visited[26]={0};//visited characters
-        int n=s.length();
-        
-        //stores count of each character
-        for(int i=0;i<n;i++){
-            count[s[i]-'a']++;
+       int freq[26] = {0};
+       int n = s.length();
+       unordered_set<char> Set;
+       for(int i = 0; i < n; ++i) {
+            freq[s[i]-'a']++;
         }
-        string res="";//stores the resultant string
-        for(int i=0;i<n;i++){
-            //decrease the count of current character
-            count[s[i]-'a']--;
-            //if character is not already in answer
-            if(!visited[s[i]-'a']){
-                //last chracter>s[i] and its count >0
-                while(res.length()>0 && res.back()>s[i] && count[res.back()-'a']>0){
-                    //mark letter unvisited
-                    visited[res.back()-'a']=0;
-                    res.pop_back();
-                }
-                //add s[i] in res and mark it visited
-                res+=s[i];
-                visited[s[i]-'a']=1;
+        
+        stack<char> st;
+        for(int i = 0; i < n; ++i) {
+            freq[s[i]-'a']--;
+            if(Set.count(s[i]))
+                continue;
+            
+            while(!st.empty() && st.top() > s[i] && freq[st.top()-'a'] > 0) {
+                Set.erase(st.top());
+                st.pop();
+            }
+            
+            if(Set.count(s[i]) == 0) { 
+                st.push(s[i]);
+                Set.insert(s[i]);
             }
         }
-        return res;
+        
+        string str = "";
+        while(!st.empty()) {
+            str += st.top();
+            st.pop();
+        }
+        reverse(str.begin(), str.end());
+        return str;
     }
 };
