facde88a2eb05167e421a083b8704149484f8b92
TRIVENDRAM PAL <43501679+tri10256@users.noreply.github.com>
2022-03-12 20:30:06 +0530

    Time: 84 ms (11.53%), Space: 155.8 MB (5.54%) - LeetHub

--- 650-2-keys-keyboard/650-2-keys-keyboard.cpp
+++ 650-2-keys-keyboard/650-2-keys-keyboard.cpp
@@ -5,9 +5,7 @@ public:
     int recur(int n,int i,int prev){
         if(i == n)
             return 0;
-        
         int ch1 = INT_MAX, ch2 = INT_MAX;
-        
         if(dp[i][prev] != -1)
             return dp[i][prev];
         
@@ -29,7 +27,7 @@ public:
         return dp[i][prev] = min(ch1,ch2);
     }
     int minSteps(int n) {
-        dp.resize(1001,vector<int>(1001,-1));
+        dp.resize(n+1,vector<int>(n+1,-1));
         return recur(n,1,0);
     }
 };
