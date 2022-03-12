d0e0847daaabc14582a2143f3661fbe85ad3e07f
TRIVENDRAM PAL <43501679+tri10256@users.noreply.github.com>
2022-03-12 20:22:13 +0530

    Time: 251 ms (5.03%), Space: 387 MB (5.54%) - LeetHub

---
+++ 650-2-keys-keyboard/650-2-keys-keyboard.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    
+    vector<vector<int>>dp;
+    int recur(int n,int i,int prev){
+        if(i == n)
+            return 0;
+        
+        int ch1 = INT_MAX, ch2 = INT_MAX;
+        
+        if(dp[i][prev] != -1)
+            return dp[i][prev];
+        
+        if(2*i<=n){
+            int ans = recur(n,2*i,i);
+            if(ans == INT_MAX)
+                ch1 = INT_MAX;
+            else
+                ch1 = 2 + ans;
+        }
+        if(prev != 0 && i+prev <= n){
+            int ans = recur(n,i+prev,prev);
+            if(ans == INT_MAX)
+                ch2 = INT_MAX;
+            else
+                ch2 = ans + 1;
+        }
+        
+        return dp[i][prev] = min(ch1,ch2);
+    }
+    int minSteps(int n) {
+        dp.resize(1001,vector<int>(1001,-1));
+        return recur(n,1,0);
+    }
+};
