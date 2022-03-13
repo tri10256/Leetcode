25d31118e24910002185b06b06f440b02127b39f
TRIVENDRAM PAL <43501679+tri10256@users.noreply.github.com>
2022-03-13 13:29:28 +0530

    Time: 170 ms (12.50%), Space: 63.4 MB (75.00%) - LeetHub

--- 2202-maximize-the-topmost-element-after-k-moves/2202-maximize-the-topmost-element-after-k-moves.cpp
+++ 2202-maximize-the-topmost-element-after-k-moves/2202-maximize-the-topmost-element-after-k-moves.cpp
@@ -1,48 +1,12 @@
 class Solution {
 public:
-    int maximumTop(vector<int>& nums, int k) {
-        priority_queue<int>pq;
-        stack<int>st;
-        
-        int n = nums.size();
-        for(int i = nums.size()-1;i>=0;i--)
-            st.push(nums[i]);
-        
-        if(k%2 != 0 && nums.size() == 1)
-            return -1;
-        
-        if(k > n ){
-            int maxi = *max_element(nums.begin(),nums.end());
-            pq.push(maxi);
-            k = k-1;
-        }
-        
-        if(k!=n)
-        k = k%nums.size();
-        
-        int steps = k-1;
-        if(steps == 0){
-            while(k--){
-                st.pop();
-            }
-            if(st.empty())
-            return -1;
-        }
-        
-        while(steps > 0){
-            pq.push(st.top());
-            st.pop();
-            steps--;
-        }
-        
-        if(pq.size()>0){
-        st.pop();
-        if(st.empty())
-            st.push(pq.top());
-        else if(pq.top() > st.top())
-        st.push(pq.top());
-        }
-        
-        return st.top();
+    int maximumTop(vector<int>& A, int k) {
+        int N = A.size();
+        if (k == 0) return N >= 1 ? A[0] : -1; // if no moves allowed, return the topmost element if any
+        if (k == 1) return N == 1 ? -1 : A[1]; // if only one move is allowed, we can only remove the topmost element
+        if (N == 1) return k % 2 == 0 ? A[0] : -1; // if `N == 1`, we can return the topmost element if `k` is a even number (keep removing the topmost element and adding it back).
+        int mx = *max_element(begin(A), begin(A) + min(k - 1, N)); // we can take `min(k-1, N)` elements and put back the largest one on the top
+        if (k < N) mx = max(mx, A[k]); // If `k < N`, we can take all the topmost `k` elements and return the one left at the top
+        return mx;
     }
 };
