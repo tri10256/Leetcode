cc999b6bb066057ed20cf592f33d1f32e3624314
TRIVENDRAM PAL <43501679+tri10256@users.noreply.github.com>
2022-03-14 13:08:30 +0530

    Time: 648 ms (71.43%), Space: 139.5 MB (57.14%) - LeetHub

---
+++ 2203-minimum-weighted-subgraph-with-the-required-paths/2203-minimum-weighted-subgraph-with-the-required-paths.cpp
@@ -0,0 +1,66 @@
+class Solution {
+public:
+    
+    void shortestPath(int src,int n,vector<long long>&dist, vector<vector<pair<int,int>>>&graph){
+        
+        priority_queue<pair<long long,int>,vector<pair<long long ,int>>,greater<pair<long long,int>>>pq;
+        pq.push({0,src});
+        
+        while(!pq.empty()){
+            auto u = pq.top().second;
+           auto distance = pq.top().first;
+            pq.pop();
+            if(dist[u] != distance)
+                continue;
+           
+            
+            for(auto &next:graph[u]){
+                auto w = next.first;
+                auto v = next.second;
+                
+                if(dist[v] > distance + w){
+                    dist[v] = distance + w;
+                    pq.push({dist[v],v});
+                }
+            }
+        }
+    
+    }
+    
+    long long minimumWeight(int n, vector<vector<int>>& edges, int src1, int src2, int dest) {
+        
+        vector<vector<pair<int,int>>>graphforward(n);
+        vector<vector<pair<int,int>>>graphreverse(n);
+        long long max_val = 10000000000, res = LLONG_MAX;
+          
+        for(auto &edge:edges){
+            int u = edge[0];
+            int v = edge[1];
+            int w = edge[2];
+            
+            graphforward[u].push_back({w,v});
+            graphreverse[v].push_back({w,u});
+        }
+        
+        vector<long long>dist1(n,max_val);
+        dist1[src1] = 0;
+        shortestPath(src1,n,dist1,graphforward);
+        
+        vector<long long>dist2(n,max_val);
+        dist2[src2] = 0;
+        shortestPath(src2,n,dist2,graphforward);
+        
+        vector<long long>distdest(n,max_val);
+        distdest[dest] = 0;
+        shortestPath(dest,n,distdest,graphreverse);
+        
+        if(distdest[src1] == max_val || distdest[src2] == max_val)
+            return -1;
+        
+        for(int i = 0;i<n;i++){
+            res = min(res,distdest[i] + dist1[i] + dist2[i]);
+        }
+        
+        return res;
+    }
+};
