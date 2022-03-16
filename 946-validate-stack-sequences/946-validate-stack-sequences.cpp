f3c65c3170d56ae4e2a8ba40159fdfaf767c42fd
TRIVENDRAM PAL <43501679+tri10256@users.noreply.github.com>
2022-03-17 01:41:44 +0530

    Time: 10 ms (70.27%), Space: 15.4 MB (15.78%) - LeetHub

---
+++ 946-validate-stack-sequences/946-validate-stack-sequences.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    bool validateStackSequences(vector<int>& pushed, vector<int>& popped) {
+        int n = pushed.size();
+        stack<int>st;
+        int j = 0;
+        for(auto val:pushed){
+            st.push(val);
+            while(!st.empty() && j <n && st.top() == popped[j]){
+                st.pop();
+                j++;
+            }
+        }
+        
+        return j == n;
+    }
+};
