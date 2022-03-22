6fbb6c7829c42f0fd0d2a14545e4668fe4f2c2da
TRIVENDRAM PAL <43501679+tri10256@users.noreply.github.com>
2022-03-22 17:24:09 +0530

    Time: 29 ms (48.05%), Space: 18.4 MB (83.63%) - LeetHub

---
+++ 845-longest-mountain-in-array/845-longest-mountain-in-array.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    int longestMountain(vector<int>& arr) {
+        int len = arr.size();
+        int maxi = 0;
+        int i = 1;
+        
+        while(i<len-1){
+            
+            if(arr[i] > arr[i-1] && arr[i] > arr[i+1]){
+                
+                int low = i-1;
+                int high = i+1;
+                
+                while(low >= 0 && arr[low] < arr[low+1])
+                    low--;
+                
+                while(high < len && arr[high-1] > arr[high])
+                    high++;
+                
+                maxi = max(maxi,high - low - 1);
+                
+                i = high;
+                
+            }else{
+                i++;
+            }
+        }
+        
+        return maxi;
+    }
+};
