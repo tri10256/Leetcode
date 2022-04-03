103815719142e1f282b4921acbabe716d988b1b7
Trivendram Pal <43501679+tri10256@users.noreply.github.com>
2022-04-03 07:43:49 +0530

    Time: 3 ms (87.24%), Space: 12.2 MB (37.25%) - LeetHub

--- 31-next-permutation/31-next-permutation.cpp
+++ 31-next-permutation/31-next-permutation.cpp
@@ -1,31 +1,75 @@
 class Solution {
 public:
     void nextPermutation(vector<int>& nums) {
-        ///step1=traverse from back find a number smaller than prev;
-        ///step2 = traverse from back and find a number greater than number find         ////in stpe1;
-        ///if then swap the both numbers and reverse as from the step1+1 index to  /////////last to find the lexicographically smaller
-        //// 
+        //step1 =traverse from back find a number smaller than prev;
+        //step2 = traverse from back and find a number greater than number find         ////in stpe1;
+        //if then swap the both numbers and reverse as from the step1+1 index to  /////////last to find the //lexicographically smaller
         
-        int n = nums.size(),k,l;
-        
-        for(k= n-2;k>=0;k--){
+//         int n = nums.size(),k,l;
+//         for(k= n-2;k>=0;k--){
             
-            if(nums[k]<nums[k+1])
+//             if(nums[k]<nums[k+1])
+//                 break;
+//         }
+        
+//         if(k<0){
+//             reverse(nums.begin(),nums.end());
+//         }else{
+            
+//             for(l = n-1;l>=0;l--){
+//                 if(nums[l]>nums[k]){
+//                     break;
+//                 }
+//             }
+//             swap(nums[l],nums[k]);
+//             reverse(nums.begin()+k+1,nums.end());
+//         }
+        
+        int n = nums.size();
+        int k,l;
+        
+        for(k = n-2;k>=0;k--){
+            if(nums[k] < nums[k+1])
                 break;
         }
         
-        if(k<0){
+        if(k < 0)
             reverse(nums.begin(),nums.end());
-        }else{
+        else{
             
-            for(l = n-1;l>=0;l--){
-                if(nums[l]>nums[k]){
+            for( l = n-1;l>=0;l--){
+                if(nums[l] > nums[k])
                     break;
-                }
             }
+            
             swap(nums[l],nums[k]);
             reverse(nums.begin()+k+1,nums.end());
         }
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
         
     }
 };
