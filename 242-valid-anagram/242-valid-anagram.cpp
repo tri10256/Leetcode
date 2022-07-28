eb0d5548995a389b0a301172dee9dc501170dfec
Trivendram Pal <43501679+tri10256@users.noreply.github.com>
2022-07-28 15:45:21 +0530

    Time: 8 ms (82.77%), Space: 7.4 MB (45.59%) - LeetHub

---
+++ 242-valid-anagram/242-valid-anagram.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        unordered_map<char,int>mp;
+        for(int i=0;i<s.size();i++)
+        {
+            mp[s[i]]++;
+        }
+        
+        for(int i=0;i<t.size();i++)
+        {
+            mp[t[i]]--;
+        }
+        for(auto i:mp)
+            if(i.second!=0)
+                return false;
+        return true;
+        
+        
+    }
+};
